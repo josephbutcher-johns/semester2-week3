a1f51760b5807edbbfbac6335121bc41564f3ec5
josephbutcher-johns <rpkj0632@leeds.ac.uk>
2026-02-16 14:26:35 +0000

    updated

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -10,19 +10,18 @@
  int main( void ) {
 
     // define and initialise variables for the problem data 
-   float salary = 36250.00;
-   float NIRate = 0.08;
-   float TAXRate = 0.15;
+    float salary = 36250.00;
+    float NIRate = 0.08;
+    float TAXRate = 0.15;
     // calculate the deductions and final take-home salary
-   float NIDeduction = salary*NIRate;
-   float TAXDeduction = salary - 12500;
-   TAXDeduction = TAXDeduction * TAXRate;
-   float TakeHome = salary - (TAXDeduction + NIDeduction);
+    float NIDeduction = salary * NIRate;
+    float TAXDeduction = (salary - 12500) * TAXRate;
+    float TakeHome = salary - (NIDeduction + TAXDeduction);
     // Use only these print statement with appropriate formatting and variable names
     printf("Salary £%.2f\n", salary);
     printf("NI contribution £%.2f\n", NIDeduction);
     printf("Tax contribution £%.2f\n", TAXDeduction);
     printf("Take home salary £%.2f\n", TakeHome);
 
-    return 0;
+   return 0;
  }
